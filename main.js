7001f71430afc00972876ca6c9a294ef00952f9d
gubsey <118205572+gubsey@users.noreply.github.com>
2024-10-24 19:35:23 +0000

    xlsx to json woohoo

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ import Alpine from 'alpinejs'
 import './style.css'
 import initSqlJs from 'sql.js'
 import wasm from 'sql.js/dist/sql-wasm.wasm?url'
-import { themes } from './daisy_themes'
+import * as XLSX from 'xlsx'
+/**
+ * @param {File} file 
+ */
+window.xlsx_to_obj = async (file) => {
+  const xl = XLSX.read(await file.arrayBuffer())
+  return xl.SheetNames.map(name => [name, XLSX.utils.sheet_to_json(xl.Sheets[name])])
+}
 
 try {
   const sql = await initSqlJs({
@@ -14,11 +21,10 @@ try {
     insert into fruits values ("apples", 3), ("bananas", 5), ("grapes", 7); \
   ')
 
-  
 
   window.db = db
   window.Alpine = Alpine
-  window.daisy_themes = themes
+  window.xlsx = XLSX
 
   Alpine.start()
 } catch (e) {
